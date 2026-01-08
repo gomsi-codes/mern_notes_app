73a10a8abe41261ef365dc7fe3ae94a71d1ae7b2
Gomsi <gomsi000005@gmail.com>
2026-01-08 19:14:17 +0530

    Rename import for Homepage to HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
-import Homepage from './pages/Homepage'
+import Homepage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetailPage from './pages/NoteDetailPage'
 function App() {
